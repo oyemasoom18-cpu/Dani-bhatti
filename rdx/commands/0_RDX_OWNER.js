68421d4ea144da4f2d6e5e9a02029102991cb19e
ffrfacebookmarketingytpremium-ops <ffrfacebookmarketingytpremium@gmail.com>
2025-12-26 15:58:39 +0400

    Update 0_RDX_OWNER.js

--- rdx/commands/0_RDX_OWNER.js
+++ rdx/commands/0_RDX_OWNER.js
@@ -17,7 +17,7 @@ module.exports = {
     const { threadID, messageID } = event;
 
     const ownerPics = [
-      'https://i.ibb.co/Cp13xBsR/b2edd4c03615.jpg',
+      'https://i.imgur.com/Mt9xt6d.jpeg',
       '',
       '',
       ''
